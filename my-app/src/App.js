b1a94a80d719b1d3a34084aaa3ee1d93e4a79bb6
markbrowne <markbrownee@gmail.com>
2016-09-03 20:08:37 +0100

    create-react

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+var HelloUser = React.createClass({
+  getInitialState: function(){
+    return {
+      username: '@tylermcginnis33'
+    }
+  },
+  handleChange: function(e){
+    this.setState({
+      username: e.target.value
+    });
+  },
+  render: function(){
+    return (
+      <div>
+        Hello {this.state.username} <br />
+        Change Name: <input type="text" value={this.state.username} onChange={this.handleChange} />
+      </div>
+    )
+  }
+});   
+  
+
+
+export default HelloUser;
